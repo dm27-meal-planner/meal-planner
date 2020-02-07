b4091b89d1935a04d3064e2255b3a0a23021dfba
Jerry Chen <raychen31638@gmail.com>
2020-02-07 10:38:20 -0600

    components init

---
+++ src/Components/Fridge/Fridge.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Fridge extends Component{
+    constructor(){
+        super();
+    }
+    render(){
+        return (
+            <div>
+                Fridge
+            </div>
+        )
+    }
+}
+export default Fridge;
---
+++ src/Components/GroceryList/GroceryList.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class GroceryList extends Component{
+    constructor(){
+        super();
+    }
+    render(){
+        return (
+            <div>
+                GroceryList
+            </div>
+        )
+    }
+}
+export default GroceryList;
