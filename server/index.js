71fe18742577b7431c692b8471ab805826f53bee
Saul Maldonado <samaldonado4@gmail.com>
2020-02-07 11:33:00 -0600

    ingredients controller

--- server/index.js
+++ server/index.js
@@ -28,8 +28,42 @@ massive(CONNECTION_STRING).then(db => {
    console.log('Connected to database');
 })
 
-// endpoints
+//ingredient endpoints
 app.post('/api/ingredient', ing.searchIngredient)
 app.post('/api/ingredient', )
+
+//user endpoints
+// app.get('user/getuser', getUser);
+// app.post('user/registeruser', registerUser);
+// app.post('user/login', loginUser);
+
+//mealplan endpoints
+//momentjs can format weeks into numbers
+// app.get(`api/mealplan/:usre_id?week=${weeknumber}`, getUserMeals);
+// app.post('api/mealplan/:user_id', addMeal);
+// app.put('api/mealplan/:meal_id', editMeal);
+// app.delete('api/mealplan/:meal_id', deleteMeal);
+
+//recipe endpoints
+// app.get('api/recipe/mostliked', getMostLikedRecipe);
+// app.get('api/recipe/recentlyadded', getRecentRecipe);
+// app.get('api/recipe/userrecipe/:user_id', getUserRecipe);
+// app.get('api/recipe/:recipe_id', getRecipeById);
+// app.post('api/recipe/addrecipe', addRecipe);
+// app.delete('api/recipe/:recipe_id', deleteRecipe);
+// app.put('api/recipe/editrecipe/:recipe_id', editRecipe);
+// app.get(`api/recipe/search?${searchinfohere}`, getRecipeByQuery);
+
+//fridge endpoints
+// app.get('api/fridge/:user_id', getUserFridge);
+// app.post('api/fridge/:user_id', addItem);
+// app.put(`api/fridge/:user_id?item=${item_id}`, editItem);
+// app.delete(`api/fridge/:user_id?item=${item_id}`, deleteItem);
+
+//grocerylist endpoints
+// app.get('api/grocerylist/:user_id', getUserGroceryList);
+// app.post('api/grocerylist/:user_id', addItemToList);
+// app.put('api/grocerylist/:user_id', editGroceryList);
+// app.delete(`api/grocerylist/:user_id?item=${item_id}`, deleteGroceryItem);
 
 app.listen(SERVER_PORT, () => console.log(`Server listening on ${SERVER_PORT}`));
