5370c8cbb816afa65d9119aa9ee2af51919660c3
Gabriel Kim <gabriel.kimsfweb@gmail.com>
2020-02-10 12:23:13 -0600

    update env

--- server/firebase/firebase.js
+++ server/firebase/firebase.js
@@ -1,20 +1,23 @@
 // Firebase App (the core Firebase SDK) is always required and
 // must be listed before other Firebase SDKs
 import * as firebase from "firebase/app";
+require('dotenv').config();
 
 // Add the Firebase services that you want to use
 // We only want to use Firebase Auth here
 import "firebase/auth";
 
+const {FIREBASE_API_KEY, FIREBASE_AUTH_DOMAIN, FIREBASE_DATABASE_URL, FIREBASE_PROJECT_ID, FIREBASE_STORAGE_BUCKET, FIREBASE_MESSAGING_SENDER_ID, FIREBASE_APP_ID} = process.env;
+
 // Your app's Firebase configuration
 var firebaseConfig = {
-   apiKey: "AIzaSyDWcEDSNOv7ICeM19lQTglBrkDt4SxplSs",
-   authDomain: "mealplan-ba9b9.firebaseapp.com",
-   databaseURL: "https://mealplan-ba9b9.firebaseio.com",
-   projectId: "mealplan-ba9b9",
-   storageBucket: "mealplan-ba9b9.appspot.com",
-   messagingSenderId: "559367326575",
-   appId: "1:559367326575:web:4c7a9b33b190a87a89f69a"
+   apiKey: FIREBASE_API_KEY,
+   authDomain: FIREBASE_AUTH_DOMAIN,
+   databaseURL: FIREBASE_DATABASE_URL,
+   projectId: FIREBASE_PROJECT_ID,
+   storageBucket: FIREBASE_STORAGE_BUCKET,
+   messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+   appId: FIREBASE_APP_ID
 };
 
 // Initialize Firebase
