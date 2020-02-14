6b2816b51bf748632b6f4c8d1a53321c4c4cf6dc
Gabriel Kim <gabriel.kimsfweb@gmail.com>
2020-02-14 08:53:35 -0600

    add item to list db

--- server/controllers/grocerylistcontroller.js
+++ server/controllers/grocerylistcontroller.js
@@ -8,12 +8,13 @@ const getUserGroceryList = async (req, res) => {
 const addItemToList = async (req, res) => {
    const {user_id} = req.params;
    const db = req.app.get('db');
+   console.log(req.body);
    const userGroceryList = await req.body.map(element => {
       // price is given in cents 
       element.price = (element.price / 100).toFixed(2);
       // image link
       element.image = `https://www.spoonacular.com/cdn/ingredients_100x100/${element.image}`
-      return db.list_items.add_item(element.quantity, element.unit, user_id, element.id, element.price, element.name, element.image)
+      return db.list_items.add_item(element.amount, element.unit, user_id, element.id, element.price, element.name, element.image)
    })
    res.status(200).json(userGroceryList);
 }
